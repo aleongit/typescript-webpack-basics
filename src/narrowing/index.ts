eefcf928990e85b102baedeb1ac7bd8081fee0a3
aleongit <aleix.leon@gmail.com>
2023-08-16 17:25:35 +0200

    fix: tutorial

--- src/narrowing/index.ts
+++ src/narrowing/index.ts
@@ -1151,37 +1151,6 @@ const never = () => {
 
   */
 
-  interface Circle {
-    kind: 'circle';
-    radius: number;
-  }
-
-  interface Square {
-    kind: 'square';
-    sideLength: number;
-  }
-
-  interface Triangle {
-    kind: 'triangle';
-    sideLength: number;
-  }
-
-  type Shape = Circle | Square;
-  //type Shape = Circle | Square | Triangle;
-
-  function getArea(shape: Shape) {
-    switch (shape.kind) {
-      case 'circle':
-        return Math.PI * shape.radius ** 2;
-      case 'square':
-        return shape.sideLength ** 2;
-      //error de _exhaustiveCheck si no hi ha cas Triangle
-      default:
-        const _exhaustiveCheck: never = shape;
-        return _exhaustiveCheck;
-    }
-  }
-
   sortida += `<code>
   <span class="ressalta">//Adding a new member to the Shape union, will cause a TypeScript error</span><br>
   interface Triangle {<br>
