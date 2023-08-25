6a6769c1c5c280001053ac197f7edce4e2e387e3
aleongit <aleix.leon@gmail.com>
2023-08-25 13:20:54 +0200

    feat: type-manipulation tutorial

--- src/manipulation/index.ts
+++ src/manipulation/index.ts
@@ -1964,12 +1964,410 @@ const templateLiteralTypes = (title: string) => {
   */
   let sortida = `<h2 id="${title}">${title}</h2>`;
 
+  /*
+  Template literal types build on string literal types, 
+  and have the ability to expand into many strings via unions.
+
+  They have the same syntax as template literal strings in JavaScript, but are used in type positions.
+  When used with concrete literal types, 
+  a template literal produces a new string literal type by concatenating the contents.
+
+  type World = "world";
+  type Greeting = `hello ${World}`;
+    //type Greeting = "hello world"
+
+
+  When a union is used in the interpolated position, 
+  the type is the set of every possible string literal that could be represented by each union member:
+
+  type EmailLocaleIDs = "welcome_email" | "email_heading";
+  type FooterLocaleIDs = "footer_title" | "footer_sendoff";
+  
+  type AllLocaleIDs = `${EmailLocaleIDs | FooterLocaleIDs}_id`;
+    //type AllLocaleIDs = "welcome_email_id" | "email_heading_id" | "footer_title_id" | "footer_sendoff_id"
+
+
+  For each interpolated position in the template literal, the unions are cross multiplied:
+  
+  type AllLocaleIDs = `${EmailLocaleIDs | FooterLocaleIDs}_id`;
+  type Lang = "en" | "ja" | "pt";
+  
+  type LocaleMessageIDs = `${Lang}_${AllLocaleIDs}`;
+    //type LocaleMessageIDs = "en_welcome_email_id" | "en_email_heading_id" | "en_footer_title_id" | "en_footer_sendoff_id" | "ja_welcome_email_id" | "ja_email_heading_id" | "ja_footer_title_id" | "ja_footer_sendoff_id" | "pt_welcome_email_id" | "pt_email_heading_id" | "pt_footer_title_id" | "pt_footer_sendoff_id"
+
+  We generally recommend that people use ahead-of-time generation for large string unions, but this is useful in smaller cases.
+  */
+
+  type World = 'world';
+  type Greeting = `hello ${World}`;
+  //type Greeting = "hello world"
+
+  type EmailLocaleIDs = 'welcome_email' | 'email_heading';
+  type FooterLocaleIDs = 'footer_title' | 'footer_sendoff';
+  type AllLocaleIDs = `${EmailLocaleIDs | FooterLocaleIDs}_id`;
+  //type AllLocaleIDs = "welcome_email_id" | "email_heading_id" | "footer_title_id" | "footer_sendoff_id"
+
+  type Lang = 'en' | 'ja' | 'pt';
+  type LocaleMessageIDs = `${Lang}_${AllLocaleIDs}`;
+  //type LocaleMessageIDs = "en_welcome_email_id" | "en_email_heading_id" | "en_footer_title_id" |
+  //"en_footer_sendoff_id" | "ja_welcome_email_id" | "ja_email_heading_id" | "ja_footer_title_id" |
+  //"ja_footer_sendoff_id" | "pt_welcome_email_id" | "pt_email_heading_id" | "pt_footer_title_id" |
+  //"pt_footer_sendoff_id"
+
   sortida += `<code>
-  test
+  <b>//Template literal types build on string literal types,<br> 
+  and have the ability to expand into many strings via unions</b><br><br>
+  <b>//a template literal produces a new string literal type by concatenating the contents</b><br>
+  type World = 'world';<br>
+  type Greeting = <mark>${'`hello ${World}`'}</mark>;<br>
+  <b>//type Greeting = "hello world"</b><br><br>
+  <b>//When a union is used in the interpolated position,<br>
+  the type is the set of every possible string literal that could be represented by each union member</b><br>
+  type EmailLocaleIDs = 'welcome_email' | 'email_heading';<br>
+  type FooterLocaleIDs = 'footer_title' | 'footer_sendoff';<br>
+  type AllLocaleIDs = <mark>${'`${EmailLocaleIDs | FooterLocaleIDs}_id`'}</mark>;<br>
+  <b>//type AllLocaleIDs = "welcome_email_id" | "email_heading_id" | "footer_title_id" | "footer_sendoff_id"</b><br><br>
+  <b>//For each interpolated position in the template literal, the unions are cross multiplied</b><br>
+  type Lang = 'en' | 'ja' | 'pt';<br>
+  type LocaleMessageIDs = <mark>${'`${Lang}_${AllLocaleIDs}`'}</mark>;<br>
+  <b>//type LocaleMessageIDs = "en_welcome_email_id" | "en_email_heading_id" | "en_footer_title_id" |<br>
+  //"en_footer_sendoff_id" | "ja_welcome_email_id" | "ja_email_heading_id" | "ja_footer_title_id" |<br>
+  //"ja_footer_sendoff_id" | "pt_welcome_email_id" | "pt_email_heading_id" | "pt_footer_title_id" |<br>
+  //"pt_footer_sendoff_id"</b><br>
   </code>`;
 
   /*
-   */
+  String Unions in Types
+  */
+
+  sortida += '<h3>String Unions in Types</h3>';
+
+  /*
+  The power in template literals comes when defining a new string based on information inside a type.
+
+  Consider the case where a function (makeWatchedObject) 
+  adds a new function called on() to a passed object. 
+  In JavaScript, its call might look like: makeWatchedObject(baseObject). 
+  
+  We can imagine the base object as looking like:
+
+  const passedObject = {
+    firstName: "Saoirse",
+    lastName: "Ronan",
+    age: 26,
+  };
+
+  The on function that will be added to the base object expects two arguments, 
+  an eventName (a string) and a callback (a function).
+
+  The eventName should be of the form attributeInThePassedObject + "Changed"; 
+  thus, firstNameChanged as derived from the attribute firstName in the base object.
+
+  The callback function, when called:
+
+  - Should be passed a value of the type associated with the name attributeInThePassedObject; 
+  thus, since firstName is typed as string, 
+  the callback for the firstNameChanged event expects a string to be passed to it at call time. 
+  Similarly events associated with age should expect to be called with a number argument
+  
+  - Should have void return type (for simplicity of demonstration)
+
+  The naive function signature of on() might thus be: 
+  on(eventName: string, callback: (newValue: any) => void). 
+  
+  However, in the preceding description, 
+  we identified important type constraints that we’d like to document in our code. 
+  Template Literal types let us bring these constraints into our code.
+
+  const person = makeWatchedObject({
+  firstName: "Saoirse",
+  lastName: "Ronan",
+  age: 26,
+  });
+  
+  // makeWatchedObject has added `on` to the anonymous Object
+  
+  person.on("firstNameChanged", (newValue) => {
+    console.log(`firstName was changed to ${newValue}!`);
+  });
+
+  Notice that on listens on the event "firstNameChanged", not just "firstName". 
+  Our naive specification of on() could be made more robust if we were to ensure 
+  that the set of eligible event names was constrained by the union of attribute names 
+  in the watched object with “Changed” added at the end. 
+  While we are comfortable with doing such a calculation in JavaScript 
+  i.e. Object.keys(passedObject).map(x => `${x}Changed`), 
+  template literals inside the type system provide a similar approach to string manipulation:
+
+  type PropEventSource<Type> = {
+      on(eventName: `${string & keyof Type}Changed`, callback: (newValue: any) => void): void;
+  };
+  
+  /// Create a "watched object" with an `on` method
+  /// so that you can watch for changes to properties.
+  declare function makeWatchedObject<Type>(obj: Type): Type & PropEventSource<Type>;
+
+  With this, we can build something that errors when given the wrong property:
+
+  const person = makeWatchedObject({
+    firstName: "Saoirse",
+    lastName: "Ronan",
+    age: 26
+  });
+  
+  person.on("firstNameChanged", () => {});
+  
+  // Prevent easy human error (using the key instead of the event name)
+  person.on("firstName", () => {});
+  Argument of type '"firstName"' is not assignable to parameter of type '"firstNameChanged" | "lastNameChanged" | "ageChanged"'.
+  
+  // It's typo-resistant
+  person.on("frstNameChanged", () => {});
+  Argument of type '"frstNameChanged"' is not assignable to parameter of type '"firstNameChanged" | "lastNameChanged" | "ageChanged"'.
+
+
+  Here we made on into a generic method.
+
+  When a user calls with the string "firstNameChanged", 
+  TypeScript will try to infer the right type for Key. 
+  To do that, it will match Key against the content before "Changed" and infer the string "firstName". 
+  Once TypeScript figures that out, 
+  the on method can fetch the type of firstName on the original object, 
+  which is string in this case. Similarly, when called with "ageChanged", 
+  TypeScript finds the type for the property age which is number.
+
+  Inference can be combined in different ways, often to deconstruct strings, 
+  and reconstruct them in different ways.
+
+
+  */
+
+  sortida += `<code>
+  <b>//example on()</b><br>
+  type PropEventSource<Type> = {<br>
+      &nbsp;<mark>on</mark>(eventName: <mark>${'`${string & keyof Type}Changed`'}</mark>, callback: (newValue: any) => void): void;<br>
+  };<br><br>
+  declare function makeWatchedObject&lt;T&gt;(obj: T): T & PropEventSource&lt;T&gt;;<br><br>
+  const person = makeWatchedObject({<br>
+    &nbsp;firstName: "Saoirse",<br>
+    &nbsp;lastName: "Ronan",<br>
+    &nbsp;age: 26<br>
+  });<br><br>
+  person.on("firstNameChanged", () => {}); <b>//ok</b><br><br>
+  <mark>// Prevent easy human error (using the key instead of the event name)</mark><br>
+  person.on("firstName", () => {}); <b>//error</b><br>
+  <b>//Argument of type '"firstName"' is not assignable to parameter of type '"firstNameChanged" |<br>
+  "lastNameChanged" | "ageChanged"'.</b><br><br>
+  <mark>// It's typo-resistant</mark><br>
+  person.on("frstNameChanged", () => {}); <b>//error</b><br>
+  <b>//Argument of type '"frstNameChanged"' is not assignable to parameter of type '"firstNameChanged" |<br>
+  "lastNameChanged" | "ageChanged"'.</b><br>
+  </code>`;
+
+  /*
+  Inference with Template Literals
+  */
+  sortida += '<h3>Inference with Template Literals</h3>';
+
+  /*
+  Notice that we did not benefit from all the information provided in the original passed object. 
+  Given change of a firstName (i.e. a firstNameChanged event), 
+  we should expect that the callback will receive an argument of type string. 
+  Similarly, the callback for a change to age should receive a number argument. 
+  We’re naively using any to type the callback’s argument. 
+  Again, template literal types make it possible to ensure an attribute’s data type will be 
+  the same type as that attribute’s callback’s first argument.
+
+  The key insight that makes this possible is this: we can use a function with a generic such that:
+
+  1. The literal used in the first argument is captured as a literal type
+  2. That literal type can be validated as being in the union of valid attributes in the generic
+  3. The type of the validated attribute can be looked up in the generic’s structure 
+    using Indexed Access
+  4. This typing information can then be applied to ensure the argument 
+    to the callback function is of the same type
+
+
+  type PropEventSource<Type> = {
+    on<Key extends string & keyof Type>
+        (eventName: `${Key}Changed`, callback: (newValue: Type[Key]) => void): void;
+  };
+  
+  declare function makeWatchedObject<Type>(obj: Type): Type & PropEventSource<Type>;
+  
+  const person = makeWatchedObject({
+    firstName: "Saoirse",
+    lastName: "Ronan",
+    age: 26
+  });
+  
+  person.on("firstNameChanged", newName => {                            
+  //(parameter) newName: string
+      console.log(`new name is ${newName.toUpperCase()}`);
+  });
+  
+  person.on("ageChanged", newAge => {
+  //(parameter) newAge: number
+      if (newAge < 0) {
+          console.warn("warning! negative age");
+      }
+  })
+  
+  */
+
+  sortida += `<code>
+  type PropEventSource<Type> = {<br>
+    &nbsp;<mark>on&lt;Key extends string & keyof Type&gt;</mark><br>
+      &nbsp;&nbsp;(eventName: <mark>${'`${Key}Changed`'}</mark>, callback: (newValue: Type[Key]) => void): void;<br>
+  };<br><br> 
+  declare function makeWatchedObject&lt;Type&gt;(obj: Type): Type & PropEventSource&lt;Type&gt;;<br><br>
+  const person = makeWatchedObject({<br>
+    &nbsp;firstName: "Saoirse",<br>
+    &nbsp;lastName: "Ronan",<br>
+    &nbsp;age: 26<br>
+  });<br><br>
+  person.on("firstNameChanged", newName => {<br>
+  <b>//(parameter) newName: string</b><br>
+    &nbsp;console.log(${'`new name is ${newName.toUpperCase()}`'});<br>
+  });<br>
+  person.on("ageChanged", newAge => {<br>
+  <b>//(parameter) newAge: number</b><br>
+    &nbsp;if (newAge < 0) {<br>
+      &nbsp;console.warn("warning! negative age");<br>
+    &nbsp;}<br>
+  })<br>
+  </code>`;
+
+  /*
+  Intrinsic String Manipulation Types
+  */
+  sortida += '<h3>Intrinsic String Manipulation Types</h3>';
+
+  /*
+  To help with string manipulation, 
+  TypeScript includes a set of types which can be used in string manipulation. 
+  These types come built-in to the compiler for performance 
+  and can’t be found in the .d.ts files included with TypeScript.
+  */
+
+  /*
+  Uppercase<StringType>
+  */
+  sortida += '<h4>Uppercase&lt;StringType&gt;</h4>';
+
+  /*
+  Converts each character in the string to the uppercase version.
+
+  Example
+  type Greeting = "Hello, world"
+  type ShoutyGreeting = Uppercase<Greeting>
+    //type ShoutyGreeting = "HELLO, WORLD"
+  
+  type ASCIICacheKey<Str extends string> = `ID-${Uppercase<Str>}`
+  type MainID = ASCIICacheKey<"my_app">
+    //type MainID = "ID-MY_APP"
+  */
+
+  type Greeting2 = 'Hello, world';
+  type ShoutyGreeting = Uppercase<Greeting2>;
+  //type ShoutyGreeting = "HELLO, WORLD"
+
+  type ASCIICacheKey<Str extends string> = `ID-${Uppercase<Str>}`;
+  type MainID = ASCIICacheKey<'my_app'>;
+  //type MainID = "ID-MY_APP"
+
+  sortida += `<code>
+  <b>//Converts each character in the string to the uppercase version</b><br>
+  type Greeting = 'Hello, world';<br>
+  type ShoutyGreeting = <mark>Uppercase&lt;Greeting&gt;</mark>;<br>
+  <b>//type ShoutyGreeting = "HELLO, WORLD"</b><br><br>
+  type ASCIICacheKey&lt;Str extends string&gt; = <mark>${'`ID-${Uppercase&lt;Str&gt;}`'}</mark>;<br>
+  type MainID = ASCIICacheKey&lt;'my_app'&gt;;<br>
+  <b>//type MainID = "ID-MY_APP"</b><br>
+  </code>`;
+
+  /*
+  Lowercase<StringType>
+  */
+  sortida += '<h4>Lowercase&lt;StringType&gt;</h4>';
+
+  /*
+  Converts each character in the string to the lowercase equivalent.
+
+  Example
+  type Greeting = "Hello, world"
+  type QuietGreeting = Lowercase<Greeting>
+    //type QuietGreeting = "hello, world"
+  
+  type ASCIICacheKey<Str extends string> = `id-${Lowercase<Str>}`
+  type MainID = ASCIICacheKey<"MY_APP">      
+    //type MainID = "id-my_app"
+  */
+
+  sortida += `<code>
+  <b>//Converts each character in the string to the lowercase equivalent</b><br>
+  type Greeting = 'Hello, world';<br>
+  type QuietGreeting = <mark>Lowercase&lt;Greeting&gt;</mark>;<br>
+  <b>//type QuietGreeting = "hello, world"</b><br><br>
+  </code>`;
+
+  /*
+  Capitalize<StringType>
+  */
+  sortida += '<h4>Capitalize&lt;StringType&gt;</h4>';
+
+  /*
+  Converts the first character in the string to an uppercase equivalent.
+
+  Example
+  type LowercaseGreeting = "hello, world";
+  type Greeting = Capitalize<LowercaseGreeting>;
+    //type Greeting = "Hello, world"
+  */
+
+  sortida += `<code>
+  <b>//Converts the first character in the string to an uppercase equivalent</b><br>
+  type LowercaseGreeting = "hello, world";<br>
+  type Greeting = <mark>Capitalize&lt;LowercaseGreeting&gt;</mark>;<br>
+  <b>//type Greeting = "Hello, world"</b><br>
+  </code>`;
+
+  /*
+  Uncapitalize<StringType>
+  */
+  sortida += '<h4>Uncapitalize&lt;StringType&gt;</h4>';
+
+  /*
+  Converts the first character in the string to a lowercase equivalent.
+
+  Example
+  type UppercaseGreeting = "HELLO WORLD";
+  type UncomfortableGreeting = Uncapitalize<UppercaseGreeting>;           
+  //type UncomfortableGreeting = "hELLO WORLD"
+  */
+
+  sortida += `<code>
+  <b>//Converts the first character in the string to a lowercase equivalent</b><br>
+  type UppercaseGreeting = "HELLO WORLD";<br>
+  type UncomfortableGreeting = <mark>Uncapitalize&lt;UppercaseGreeting&gt;</mark>;<br>
+  <b>//type UncomfortableGreeting = "hELLO WORLD"</b><br>
+  </code>`;
+
+  /*
+  Technical details on the intrinsic string manipulation types
+  The code, as of TypeScript 4.1, for these intrinsic functions uses 
+  the JavaScript string runtime functions directly for manipulation and are not locale aware.
+
+  function applyStringMapping(symbol: Symbol, str: string) {
+      switch (intrinsicTypeKinds.get(symbol.escapedName as string)) {
+          case IntrinsicTypeKind.Uppercase: return str.toUpperCase();
+          case IntrinsicTypeKind.Lowercase: return str.toLowerCase();
+          case IntrinsicTypeKind.Capitalize: return str.charAt(0).toUpperCase() + str.slice(1);
+          case IntrinsicTypeKind.Uncapitalize: return str.charAt(0).toLowerCase() + str.slice(1);
+      }
+      return str;
+  }
+  */
 
   return sortida;
 };
